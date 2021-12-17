b3c4f319c16967ea198e893a4c5e8a9539e5a4e1
kaneko <kane4108@yomiuri.com>
2021-12-17 17:33:03 +0900

    import

---
+++ 4.svelte
@@ -0,0 +1,15 @@
+<p>This is a paragraph.</p>
+
+<style>
+	p {
+		color: purple;
+		font-family: 'Comic Sans MS', cursive;
+		font-size: 2em;
+	}
+</style>
+
+<script>
+	import Nested from './Nested.svelte';
+</script>
+
+<Nested/>
