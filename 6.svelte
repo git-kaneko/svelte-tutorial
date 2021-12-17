dd8eae8854070684254fcfff24d8a1c2b4ffd142
kaneko <kane4108@yomiuri.com>
2021-12-17 17:50:45 +0900

    Assignments

---
+++ 6.svelte
@@ -0,0 +1,12 @@
+<script>
+	let count = 0;
+
+	function incrementCount() {
+		// event handler code goes here
+		count += 1;
+	}
+</script>
+
+<button on:click={incrementCount}>
+	Clicked {count} {count === 1 ? 'time' : 'times'}
+</button>
