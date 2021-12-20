340667cca7381bd64bc2e70cc424c646a7dfdfb0
kaneko <kane4108@yomiuri.com>
2021-12-20 15:45:34 +0900

    else-if blocks

---
+++ 9.svelte
@@ -0,0 +1,11 @@
+<script>
+    let x = 7;
+</script>
+
+{#if x > 10}
+    <p>{x} is greater then 10</p>
+{:else if 5 > x}
+    <p>{x} is less then 5</p>
+{:else}
+    <p>{x} is between 5 and 10</p>
+{/if}
